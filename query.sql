f1faff6f9a7206009b1482e458e433776717c788
Ivan Lushnykov <91260629+IJLushnykov@users.noreply.github.com>
2024-01-02 20:52:17 +0100

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 SELECT v.Name, s.Sales_in_Millions
 FROM Sales_Info s
-JOIN Video_Game v ON s.Index = v.Index
+JOIN Video_Game v ON s.Indexx = v.Indexx
 WHERE s.Region = 'EU' AND s.Sales_in_Millions > 2.0;
 
 
@@ -10,7 +10,7 @@ WHERE Year_of_Release > 2010 AND Critic_Score > 80;
 
 SELECT v.Name, v.Platform, g.Global_Sales_in_Millions
 FROM Video_Game v
-JOIN Global_Sales g ON v.Index = g.Index
+JOIN Global_Sales g ON v.Indexx = g.Indexx
 ORDER BY g.Global_Sales_in_Millions ASC;
 
 
